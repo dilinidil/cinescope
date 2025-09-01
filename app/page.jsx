9bd43e0f55020c566d2d0c0e9e6a2613f4af1f1c
dilinidil <dilinidil99@gmail.com>
2025-09-01 02:30:23 -0600

    Add Featured Movies section and Movie Cards section

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import HeaderNav from "@/components/header-nav";
+import FeaturedMovies from "@/components/home/featured-movies";
 import HeroBanner from "@/components/home/hero-banner";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeaderNav />
-      <main className="flex-1 bg-primary">
+      <main className="flex-1">
         <HeroBanner />
+        <FeaturedMovies />
       </main>
       <footer className="flex-1 bg-amber-400">Footer Section</footer>   
     </div>
